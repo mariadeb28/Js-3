f9b968e570d05f6b11b718f60a20d083457ff588
mariadeb <deborahgsilva@hotmail.com>
2024-02-19 17:18:34 -0300

    Projeto random

---
+++ Math.js
@@ -0,0 +1,18 @@
+/* Math */
+
+//const result = Math.pow (2,2)
+//console.log(result)
+
+//const result = Math.sqrt(25,2)
+//console.log(result)
+
+//const result = Math.ceil(3.14)
+//console.log(result)
+
+//const result = Math.floor (2.12)
+//console.log(result)
+
+//const result = Math.random()
+//console.log(result)
+
+// random sempre tras um numero aleatorio entre 0 e 1
